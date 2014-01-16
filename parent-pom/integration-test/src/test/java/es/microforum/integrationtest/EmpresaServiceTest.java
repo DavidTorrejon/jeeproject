4678a52fae549b16cce480c461f35af828e24162
daysa06 <epsstylus@hotmail.com>
2014-01-16 14:19:34 +0100

    20140116 EmpresaServiceTest

--- parent-pom/integration-test/src/test/java/es/microforum/integrationtest/EmpresaServiceTest.java
+++ parent-pom/integration-test/src/test/java/es/microforum/integrationtest/EmpresaServiceTest.java
@@ -3,6 +3,8 @@ package es.microforum.integrationtest;
 import static org.junit.Assert.*;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -13,16 +15,12 @@ import es.microforum.model.Empleado;
 import es.microforum.model.Empresa;
 import es.microforum.serviceapi.EmpresaService;
 
-
-
-
 //import org.junit.runner.RunWith;
 //import org.springframework.test.context.ContextConfiguration;
 //import org.springframework.test.context.TestExecutionListeners;
 //import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.context.support.GenericXmlApplicationContext;
 //import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-
 //@TestExecutionListeners({DependencyInjectionTestExecutionListener.class})
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@ContextConfiguration(locations = {"classpath:spring-data-app-context.xml"})
@@ -33,7 +31,7 @@ public class EmpresaServiceTest {
 	EmpresaService empresaService;
 	
 	Empresa empresa1;
-	Empresa empresa2;
+	List<Empresa> empresas;
 	Empleado e;
 	Set<Empleado> empleados;
 	
@@ -47,6 +45,8 @@ public class EmpresaServiceTest {
 		empresaService = ctx.getBean("springJpaEmpresaService",
 				EmpresaService.class);
 		
+		empresas=new ArrayList<Empresa>();
+		
 		e=new Empleado();
 		e.setDni("123456");
 		e.setNombre("juan");
@@ -55,41 +55,66 @@ public class EmpresaServiceTest {
 		e.setEmpleadocol("Col 1");
 		e.setSalarioAnual(1000.00);
 		
-		//Inicializarlo
-		//empleados=new Empleado();
+		empleados=new HashSet<Empleado>();
 		
 		empleados.add(e);
 		
 		empresa1=new Empresa("123456","Empresa 1","Direccion 1",new Date(12,12,2012),empleados);
 	}
 
-	
-	/*
-	@Test
-	public void testBusquedaNif() {
-		fail("Not yet implemented");
+	/*@Test
+	public void testAll(){
+		testSave();
+		testBusquedaNif();
+		testListado();
+		testModificar();
+		testDelete();
 	}*/
-	
-	@Test
+		
+	@Test	
 	public void testSave() {
 		empresaService.save(empresa1);
 		assertTrue(empresa1.getNif()!=null);
+		empresas = empresaService.findAll();
+		listaEmpresas(empresas);
+	}
+	
+	@Test
+	public void testBusquedaNif() {
+		assertTrue(empresaService.findByNif("123456")!=null);
 	}
 	
 	@Test
 	public void testListado() {
-		List<Empresa> empresas = empresaService.findAll();
+		empresas = empresaService.findAll();
 		assertTrue(empresas.size()>0);
+		listaEmpresas(empresas);
 	}
 	
-	/*@Test
-	public void testDelete() {
-		fail("Not yet implemented");
+	@Test	
+	public void testModificar() {
+		empresa1 = empresaService.findByNif("123456");
+		empresa1.setNombre("Nombre cambiado");
+		empresaService.save(empresa1);
+		assertTrue(empresa1.getNombre().equals("Nombre cambiado"));
+		empresas = empresaService.findAll();
+		listaEmpresas(empresas);
 	}
 	
 	@Test
-	public void testModificar() {
-		fail("Not yet implemented");
-	}*/
-
+	public void testDelete() {
+		empresa1 = empresaService.findByNif("123456");
+		empresaService.delete(empresa1);
+		assertTrue(empresaService.findByNif("123456")==null);
+		empresas = empresaService.findAll();
+		listaEmpresas(empresas);
+	}	
+	
+	private static void listaEmpresas(List<Empresa> empresas) {
+		System.out.println("");
+		for (Empresa empresa : empresas) {
+			System.out.println(empresa);
+			System.out.println();
+		}
+	}
 }
