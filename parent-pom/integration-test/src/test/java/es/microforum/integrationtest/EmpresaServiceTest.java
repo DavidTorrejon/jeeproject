b930d1e4d57e6d8ad66b995f619c223f3d5b8687
daysa06 <epsstylus@hotmail.com>
2014-01-17 11:31:55 +0100

    20140117 - EmpresaServiceTest Ok

--- parent-pom/integration-test/src/test/java/es/microforum/integrationtest/EmpresaServiceTest.java
+++ parent-pom/integration-test/src/test/java/es/microforum/integrationtest/EmpresaServiceTest.java
@@ -3,14 +3,10 @@ package es.microforum.integrationtest;
 import static org.junit.Assert.*;
 import java.sql.Date;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
 import org.junit.Before;
 import org.junit.Test;
 
-import es.microforum.model.Empleado;
 import es.microforum.model.Empresa;
 import es.microforum.serviceapi.EmpresaService;
 import org.junit.runner.RunWith;
@@ -31,91 +27,75 @@ public class EmpresaServiceTest {
 	
 	Empresa empresa1;
 	List<Empresa> empresas;
-	Empleado e;
-	Set<Empleado> empleados;
 	
 	@Before
 	public void setUp() throws Exception {
 		
 		empresas=new ArrayList<Empresa>();
 		
-		e=new Empleado();
-		e.setDni("123456");
-		e.setNombre("juan");
-		e.setDireccion("Calle 1");
-		e.setTipoEmpleado("Tipo 1");
-		e.setEmpleadocol("Col 1");
-		e.setSalarioAnual(1000.00);
-		
-		empleados=new HashSet<Empleado>();
-		
-		empleados.add(e);
-		
-		empresa1=new Empresa("123456","Empresa 1","Direccion 1",new Date(12,12,2012),empleados);
+		empresa1=new Empresa();
+		empresa1.setNif("123456789");
+		empresa1.setNombre("Empresa 1");
+		empresa1.setDireccionFiscal("Direccion 1");
+		empresa1.setFechaInicioActividades(new Date(1985,01,01));
+		empresa1.setVersion(1);
 	}
 		
 	@Test		
 	public void testSave() {
 		empresaService.save(empresa1);
-		assertTrue(empresa1.getNif()!=null);
+		assertTrue(empresa1.getNif()!=null);		
 		empresas = empresaService.findAll();
+		
 		listaEmpresas(empresas);
-		listaEmpleados(empresa1);
 	}
 	
 	@Test
 	public void testBusquedaNif() {
 		empresaService.save(empresa1);
-		assertTrue(empresaService.findByNif("123456")!=null);
+		assertTrue(empresaService.findByNif("123456789")!=null);
 	}
 	
 	@Test
 	public void testListado() {
 		empresaService.save(empresa1);
 		empresas = empresaService.findAll();
+		
 		assertTrue(empresas.size()>0);
 		listaEmpresas(empresas);
-		listaEmpleados(empresa1);
 	}
 	
 	@Test	
 	public void testModificar() {
 		empresaService.save(empresa1);
-		empresa1 = empresaService.findByNif("123456");
+		empresa1 = empresaService.findByNif("123456789");
 		empresa1.setNombre("Nombre cambiado");
-		empresaService.save(empresa1);
-		
+		empresaService.save(empresa1);		
 		empresas = empresaService.findAll();
+		
 		listaEmpresas(empresas);
-		listaEmpleados(empresa1);
 		assertTrue(empresa1.getNombre().equals("Nombre cambiado"));		
 	}
 	
 	@Test
 	public void testDelete() {
 		empresaService.save(empresa1);
-		empresa1 = empresaService.findByNif("123456");
-		assertTrue(empresa1.getNif()=="123456");
+		empresa1 = empresaService.findByNif("123456789");
+		
+		assertTrue(empresa1.getNif()=="123456789");
+		
 		empresaService.delete(empresa1);
-		assertTrue(empresaService.findByNif("123456")==null);
+		
+		assertTrue(empresaService.findByNif("123456789")==null);
+		
 		empresas = empresaService.findAll();
 		listaEmpresas(empresas);
-		listaEmpleados(empresa1);
 	}	
 	
 	private static void listaEmpresas(List<Empresa> empresas) {
 		System.out.println("");
 		for (Empresa empresa : empresas) {
 			System.out.println(empresa);
-			System.out.println();
-		}
-	}
-	
-	private static void listaEmpleados(Empresa empresas) {
-		Set<Empleado>e=new HashSet<Empleado>();
-		e=empresas.getEmpleados();
-		for (Empleado emp : e) {
-			System.out.println(emp.getNombre());
 			System.out.println();
 		}
 	}
