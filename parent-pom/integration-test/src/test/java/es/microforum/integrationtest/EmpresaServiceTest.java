a0e01d7ac13a9f0234948135ca01c07a50c70404
alumno <alumno@A09cli12>
2014-01-20 20:47:00 +0100

    20140120 - Añadido logger para trazas

--- parent-pom/integration-test/src/test/java/es/microforum/integrationtest/EmpresaServiceTest.java
+++ parent-pom/integration-test/src/test/java/es/microforum/integrationtest/EmpresaServiceTest.java
@@ -1,15 +1,20 @@
 package es.microforum.integrationtest;
 
 import static org.junit.Assert.*;
+
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import es.microforum.model.Empresa;
 import es.microforum.serviceapi.EmpresaService;
+
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
@@ -22,6 +27,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Transactional()
 public class EmpresaServiceTest {
 
+	private static final Logger logger = LoggerFactory.getLogger(EmpresaServiceTest.class);
+	
 	@Autowired
 	EmpresaService empresaService;
 	
@@ -39,46 +46,49 @@ public class EmpresaServiceTest {
 		empresa1.setDireccionFiscal("Direccion 1");
 		empresa1.setFechaInicioActividades(new Date(1985,01,01));
 		empresa1.setVersion(1);
+		
+		logger.info("Empresa creada - ");
 	}
 		
 	@Test		
 	public void testSave() {
+		logger.info("Test Save - ");
 		empresaService.save(empresa1);
 		assertTrue(empresa1.getNif()!=null);		
 		empresas = empresaService.findAll();
-		
-		listaEmpresas(empresas);
 	}
 	
 	@Test
 	public void testBusquedaNif() {
+		logger.info("Test Busqueda NIF - ");
 		empresaService.save(empresa1);
 		assertTrue(empresaService.findByNif("123456789")!=null);
 	}
 	
 	@Test
 	public void testListado() {
+		logger.info("Test Listado - ");
 		empresaService.save(empresa1);
 		empresas = empresaService.findAll();
 		
 		assertTrue(empresas.size()>0);
-		listaEmpresas(empresas);
 	}
 	
 	@Test	
 	public void testModificar() {
+		logger.info("Test Modificar - ");
 		empresaService.save(empresa1);
 		empresa1 = empresaService.findByNif("123456789");
 		empresa1.setNombre("Nombre cambiado");
 		empresaService.save(empresa1);		
 		empresas = empresaService.findAll();
 		
-		listaEmpresas(empresas);
 		assertTrue(empresa1.getNombre().equals("Nombre cambiado"));		
 	}
 	
 	@Test
 	public void testDelete() {
+		logger.info("Test Delete - ");
 		empresaService.save(empresa1);
 		empresa1 = empresaService.findByNif("123456789");
 		
@@ -89,14 +99,5 @@ public class EmpresaServiceTest {
 		assertTrue(empresaService.findByNif("123456789")==null);
 		
 		empresas = empresaService.findAll();
-		listaEmpresas(empresas);
 	}	
-	
-	private static void listaEmpresas(List<Empresa> empresas) {
-		System.out.println("");
-		for (Empresa empresa : empresas) {
-			System.out.println(empresa);
-			System.out.println();
-		}
-	}
 }
