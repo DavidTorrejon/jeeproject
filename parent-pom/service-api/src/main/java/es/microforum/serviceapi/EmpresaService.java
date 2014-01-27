6e5c612602cfc706b7bdf000f93f784f27e47033
alumno <alumno@A09cli12>
2014-01-27 21:15:53 +0100

    20140127 - Sorting CRUD Ok

--- parent-pom/service-api/src/main/java/es/microforum/serviceapi/EmpresaService.java
+++ parent-pom/service-api/src/main/java/es/microforum/serviceapi/EmpresaService.java
@@ -1,6 +1,9 @@
 package es.microforum.serviceapi;
 
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import es.microforum.model.Empresa;
 
 public interface EmpresaService {
@@ -16,4 +19,10 @@ public interface EmpresaService {
 
 	// Borra una empresa
 	public void delete(Empresa empresa);
+
+	// Busqueda por nombre paginado
+	Page<Empresa> findByNombre(String nombre, Pageable pageable);
+
+	// Busqueda de todos los empleados paginado
+	Page<Empresa> findAll(Pageable pageable);
 }
