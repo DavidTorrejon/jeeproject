2e7bea54a2369dcf882c46a2437db92e870d60ce
alumno <alumno@A09cli12>
2014-02-13 22:02:58 +0100

    20140213 - No funciona

---
+++ parent-pom/service-frontendSOAP/src/main/java/com/microforum/ws/jaxws/IEmpleadoWebService.java
@@ -0,0 +1,15 @@
+package com.microforum.ws.jaxws;
+
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+import es.microforum.model.Empleado;
+
+@WebService
+public interface IEmpleadoWebService {
+
+	@WebMethod(operationName = "callaumentosueldo")
+	public List<Empleado> callAumentoSueldo(double porcentaje);
+}
